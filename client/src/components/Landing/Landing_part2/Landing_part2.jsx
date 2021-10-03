17de1d6de2d192092c8ff13a38c0c5d35bfd7a8b
leejanghe <babybye777@gmail.com>
2021-10-04 03:58:12 +0900

    part1 done

---
+++ client/src/components/Landing/Landing_part2/Landing_part2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Landing_part2/Landing_part2.css'
+
+
+function Landing_part2() {
+    return (
+        <div>
+           gdgd
+        </div>
+    )
+}
+
+export default Landing_part2
